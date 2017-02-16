fb80e305c4da89ca7d451e05ecaa7d513728628e
Tommy-Lim <tlim24@gmail.com>
2017-02-16 10:04:35 -0800

    added find longest word

--- process_dict.py
+++ process_dict.py
@@ -1,5 +1,12 @@
 filename = "twl3.txt"
 file_ = open(filename)
-for line in file_.readlines(): 
+long_word = None
+for line in file_.readlines():
   line = line.strip()
-  print(line)
+  if long_word is None:
+      long_word = line
+      print(f"New long word is {long_word} with length {len(long_word)}")
+  elif len(line)>len(long_word):
+      long_word = line
+      print(f"New long word is {long_word} with length {len(long_word)}")
+print(long_word)
